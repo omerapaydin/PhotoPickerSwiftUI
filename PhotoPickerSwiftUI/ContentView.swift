9ad1281dc29070785a33216d98e4d97889967442
Ömer Apaydın <omerapaydin90@gmail.com>
2025-01-04 16:56:44 +0300

    First

--- PhotoPickerSwiftUI/ContentView.swift
+++ PhotoPickerSwiftUI/ContentView.swift
@@ -6,16 +6,36 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    
+    @State var selectedItem : [PhotosPickerItem] = []
+    @State var data : Data?
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           
+            PhotosPicker(selection: $selectedItem,maxSelectionCount: 1,matching: .images ){
+                Text("Selected")
+            }.onChange(of: selectedItem) { newValue in
+                guard let item = selectedItem.first else { return }
+                
+                item.loadTransferable(type: Data.self) { Result in
+                    switch Result {
+                    case .success(let data):
+                        self.data = data
+                    case .failure(let error):
+                        print(error)
+                    }
+                    
+                }
+            }
+            
+            
         }
-        .padding()
+    
     }
 }
 
